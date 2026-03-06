83e88895574f401f6f86239ae66ee8b729c78943
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-06 17:23:21 -0500

    feat: Crear caso de uso editar marca

--- Application/IRepository.cs
+++ Application/IRepository.cs
@@ -11,5 +11,7 @@ namespace ApplicationBusiness
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         Task AddAsync(T item);
+        Task EditAsync(T item);
+        Task DeleteAsync(int id);
     }
 }
