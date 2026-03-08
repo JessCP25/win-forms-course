0e630c448578408be47b9ee1f38e458721c27204
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-07 21:45:02 -0500

    feat: crear caso de uso editar beer

---
+++ Application/EditBeer.cs
@@ -0,0 +1,28 @@
+﻿using Entities;
+
+namespace ApplicationBusiness
+{
+    public class EditBeer
+    {
+        private readonly IRepository<Beer> _repository;
+        public EditBeer(IRepository<Beer> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ExecuteAsync(Beer beer)
+        {
+            if (string.IsNullOrEmpty(beer.Name)) 
+            {
+                throw new Exception("El nombre de la cerveza es obligatorio");
+            }
+
+            if (await _repository.GetByIdAsync(beer.Id) == null) 
+            {
+                throw new Exception("Cerveza no existente");
+            }
+
+            await _repository.EditAsync(beer);
+        }
+    }
+}
