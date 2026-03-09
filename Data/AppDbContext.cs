72468ee21119a631f1e71d230c7bfe1206a3a986
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-08 22:48:10 -0500

    feat: crear model beer

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,9 +8,11 @@ namespace Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<BrandModel> Brands { get; set; }
+        public DbSet<BeerModel> Beers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<BrandModel>().ToTable("Brand");
+            modelBuilder.Entity<BeerModel>().ToTable("Beer");
         }
     }
 }
