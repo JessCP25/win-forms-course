47c6ba83f6890f30cf7bc95508535d8a40f6e984
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-11 22:55:40 -0500

    feat: crear interface imapper

--- Application/AddBeer.cs
+++ Application/AddBeer.cs
@@ -3,16 +3,23 @@ using Entities;
 
 namespace ApplicationBusiness
 {
-    public class AddBeer
+    public class AddBeer<TAdditionalData>
     {
         private readonly IRepository<Beer> _repositoryBeer;
-        public AddBeer(IRepository<Beer> repositoryBeer)
+        private readonly IMapper<BeerDTO,Beer > _mapperEntity;
+        private readonly IMapper<BeerDTO, TAdditionalData> _mapperAdditionalData;
+        public AddBeer(IRepository<Beer> repositoryBeer, IMapper<BeerDTO, Beer> mapperEntity, IMapper<BeerDTO, TAdditionalData> mapperAdditionalData)
         {
             _repositoryBeer = repositoryBeer;
+            _mapperEntity = mapperEntity;
+            _mapperAdditionalData = mapperAdditionalData;
         }
 
-        public async Task ExecuteAsync(BeerDTO beer)
+        public async Task ExecuteAsync(BeerDTO beerDTO)
         {
+            var beer = _mapperEntity.Map(beerDTO);
+            var beerAdditionalData = _mapperAdditionalData.Map(beerDTO);
+
             if (string.IsNullOrEmpty(beer.Name))
             {
                 throw new Exception("El nombre de la cerveza es obligatorio");
---
+++ Application/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationBusiness
+{
+    public interface IMapper<TInp, TOut>
+    {
+        public TOut Map(TInp input);
+    }
+}
