7ac3a85bdf09823a9d61a9bd19a69e87b74b6a2d
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-12 23:29:55 -0500

    feat: definir mapper para 2 objetos

--- Application/GetBeerById.cs
+++ Application/GetBeerById.cs
@@ -6,15 +6,20 @@ namespace ApplicationBusiness
     public class GetBeerById<TAdditionalData>
     {
         private readonly IRepositoryAdditionalData<Beer, TAdditionalData> _beerRepository;
+        private readonly ISuperMapper<Beer, TAdditionalData, BeerDTO> _superMapper;
 
-        public GetBeerById(IRepositoryAdditionalData<Beer, TAdditionalData> beerRepository)
+        public GetBeerById(IRepositoryAdditionalData<Beer, TAdditionalData> beerRepository, ISuperMapper<Beer, TAdditionalData, BeerDTO> superMapper)
         {
             _beerRepository = beerRepository;
+            _superMapper = superMapper;
         }
 
         public async Task<BeerDTO> ExecuteAsync(int id)
         {
             var (beer, beerAdditionalData) = await _beerRepository.GetByIdAsync(id);
+            var beerDTO = _superMapper.Map(beer, beerAdditionalData);
+
+            return beerDTO;
         }
     }
 }
---
+++ Application/ISuperMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationBusiness
+{
+    public interface ISuperMapper<TInp1, TInp2, TOut>
+    {
+        public TOut Map(TInp1 inpunt1, TInp2 inpunt2);
+    }
+}
