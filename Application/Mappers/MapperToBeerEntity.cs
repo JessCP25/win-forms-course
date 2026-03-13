fd892a5f5b7e7cf1cc0cb7d91001d93ea86c2d44
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-12 22:19:32 -0500

    feat: crear mapper de beerDTO a entity beer

---
+++ Application/Mappers/MapperToBeerEntity.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationBusiness.DTOs;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationBusiness.Mappers
+{
+    public class MapperToBeerEntity: IMapper<BeerDTO, Beer>
+    {
+        public Beer Map(BeerDTO beerDTO)
+        {
+            return new Beer()
+            {
+                Id = beerDTO.Id,
+                Name = beerDTO.Name,
+                BrandId = beerDTO.BrandId,
+                Alcohol = beerDTO.Alcohol,
+            };
+        }
+    }
+}
