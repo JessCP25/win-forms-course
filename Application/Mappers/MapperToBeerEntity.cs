a87b043137f6532c3074eb7764cb71a4e10b46ac
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-12 22:26:27 -0500

    feat: crear mapper de beerDTO a beerAdditionalData

--- Application/Mappers/MapperToBeerEntity.cs
+++ Application/Mappers/MapperToBeerEntity.cs
@@ -1,10 +1,5 @@
 ﻿using ApplicationBusiness.DTOs;
 using Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ApplicationBusiness.Mappers
 {
---
+++ Repository/Mappers/MapperToBeerAdditionalData.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationBusiness;
+using ApplicationBusiness.DTOs;
+using Repository.AdditionalDataClass;
+
+namespace Repository.Mappers
+{
+    public class MapperToBeerAdditionalData: IMapper<BeerDTO, BeerAdditionalData>
+    {
+        public BeerAdditionalData Map(BeerDTO beerDTO)
+        {
+            return new BeerAdditionalData()
+            {
+                Description = beerDTO.Description,
+            };
+        }
+    }
+}
