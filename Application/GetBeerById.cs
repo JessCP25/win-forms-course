27bb4e0631fe46d9aa5f7fa5565988283b49c9da
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-12 23:19:54 -0500

    feat: crear caso de uso getBeerById

---
+++ Application/GetBeerById.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationBusiness.DTOs;
+using Entities;
+
+namespace ApplicationBusiness
+{
+    public class GetBeerById<TAdditionalData>
+    {
+        private readonly IRepositoryAdditionalData<Beer, TAdditionalData> _beerRepository;
+
+        public GetBeerById(IRepositoryAdditionalData<Beer, TAdditionalData> beerRepository)
+        {
+            _beerRepository = beerRepository;
+        }
+
+        public async Task<BeerDTO> ExecuteAsync(int id)
+        {
+            var (beer, beerAdditionalData) = await _beerRepository.GetByIdAsync(id);
+        }
+    }
+}
