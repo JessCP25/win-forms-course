d95c330b4ecfe8f6188b20883d84cbda0ddf08b7
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-12 23:35:49 -0500

    feat: crear mapper que recibe 2 objetos

---
+++ Repository/Mappers/SuperMapperToBeerDTO.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationBusiness;
+using ApplicationBusiness.DTOs;
+using Entities;
+using Repository.AdditionalDataClass;
+
+namespace Repository.Mappers
+{
+    public class SuperMapperToBeerDTO: ISuperMapper<Beer,BeerAdditionalData, BeerDTO>
+    {
+        public BeerDTO Map(Beer beer, BeerAdditionalData beerAdditionalData) 
+        {
+            return new BeerDTO()
+            {
+                Id = beer.Id,
+                Name = beer.Name,
+                BrandId = beer.BrandId,
+                Alcohol = beer.Alcohol,
+                Description = beerAdditionalData.Description,
+            };
+        }
+    }
+}
