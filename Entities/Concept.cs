fb5f32216691c47b852e01c02e8817d5275a1d47
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-13 23:21:30 -0500

    feat: crear entity concept

---
+++ Entities/Concept.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Concept
+    {
+        public int Quantity { get; }
+        public int IdBeer { get; }
+        public decimal UnitPrice { get; }
+        public decimal Price { get; }
+        public Concept(int quantity, int idBeer, decimal unitPrice)
+        {
+            Quantity = quantity;
+            IdBeer = idBeer;
+            UnitPrice = unitPrice;
+            Price = GetTotalPrice();
+        }
+        private decimal GetTotalPrice()
+            => Quantity * UnitPrice;
+    }
+}
