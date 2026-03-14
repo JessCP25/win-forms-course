7cc627e0223037db5cab86f24ab81a7d619064e1
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-13 23:10:17 -0500

    feat: crear query object

---
+++ Repository/QueryObjects/BeerWithBrandQuery.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Repository.QueryResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.QueryObjects
+{
+    public class BeerWithBrandQuery
+    {
+        private readonly AppDbContext _appDbContext;
+        public BeerWithBrandQuery(AppDbContext appDbContext )
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<BeerWithBrandResult>> GetAsync()
+            => await _appDbContext.Beers.Join(
+                _appDbContext.Brands,
+                beer => beer.BrandId,
+                brand => brand.Id,
+                (beer, brand) => new BeerWithBrandResult()
+                {
+                    Id = beer.Id,
+                    Name = beer.Name,
+                    Brand = brand.Name
+                }).ToListAsync();
+    }
+}
---
+++ Repository/QueryResults/BeerWithBrandResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.QueryResults
+{
+    public class BeerWithBrandResult
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
