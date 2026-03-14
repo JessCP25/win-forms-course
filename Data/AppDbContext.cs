79d27ff01483876b014a8137302a35994d4f440a
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-13 23:49:59 -0500

    feat: actualizar contexto con sale model y concept model

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,10 +9,20 @@ namespace Data
 
         public DbSet<BrandModel> Brands { get; set; }
         public DbSet<BeerModel> Beers { get; set; }
+        public DbSet<SaleModel> Sales { get; set; }
+        public DbSet<ConceptModel> Concepts { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<BrandModel>().ToTable("Brand");
             modelBuilder.Entity<BeerModel>().ToTable("Beer");
+            modelBuilder.Entity<SaleModel>().ToTable("Sale");
+            modelBuilder.Entity<ConceptModel>().ToTable("Concept");
+
+            modelBuilder.Entity<SaleModel>()
+                .HasMany(s => s.Concepts)
+                .WithOne()
+                .HasForeignKey(c => c.IdSale)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
