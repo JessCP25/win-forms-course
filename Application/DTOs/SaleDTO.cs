c0ee09e1817537098ba658ebd25292b8f242f454
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-21 18:59:06 -0500

    feat: crear saleDTO

---
+++ Application/DTOs/SaleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationBusiness.DTOs
+{
+    public class SaleDTO
+    {
+        public List<ConceptDTO> Concepts { get; set; }
+    }
+}
