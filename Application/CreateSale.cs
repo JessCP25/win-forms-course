f41bcbffd40bed1803d5c903b84b46018e0f05ff
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-21 20:10:39 -0500

    feat: crear caso de uso createSale

---
+++ Application/CreateSale.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationBusiness.DTOs;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationBusiness
+{
+    public class CreateSale
+    {
+        private readonly IRepositorySimple<Sale> _repository;
+        private readonly IMapper<SaleDTO, Sale> _mapper;
+
+        public CreateSale(IRepositorySimple<Sale> repository, IMapper<SaleDTO, Sale> mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task ExecuteAsync(SaleDTO saleDTO)
+        {
+            var sale = _mapper.Map(saleDTO);
+
+            if(sale.Concepts.Count == 0)
+            {
+                throw new Exception("Una venta debe tener conceptos.");
+            }
+            if(sale.Total <= 0)
+            {
+                throw new Exception("Una venta debe tener un total mayor a 0.");
+            }
+
+            await _repository.AddAsync(sale);
+        }
+    }
+}
