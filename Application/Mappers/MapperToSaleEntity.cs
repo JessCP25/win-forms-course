0ca66ef037c27aabb0ebdbd3b405100e07b4b0e5
jesscp25 <jessica.payano.solintels@outlook.com>
2026-03-21 20:46:53 -0500

    feat: crear mapper de saleDTO a entity sale

---
+++ Application/Mappers/MapperToSaleEntity.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationBusiness.DTOs;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationBusiness.Mappers
+{
+    public class MapperToSaleEntity: IMapper<SaleDTO, Sale>
+    {
+        public Sale Map(SaleDTO saleDTO)
+        {
+            var concepts = new List<Concept>();
+
+            foreach (var conceptDTO in saleDTO.Concepts)
+            {
+                concepts.Add(new Concept(conceptDTO.Quantity, conceptDTO.IdBeer, conceptDTO.UnitPrice));
+            }
+
+            return new Sale(DateTime.Now, concepts);
+        }
+    }
+}
